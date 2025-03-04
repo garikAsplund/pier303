a5cda29a339f59b02974ca15181334f10cd99dda
Garik Asplund <garik.asplund@gmail.com>
2025-03-03 16:30:21 -0800

    making progress on homepage

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -23,7 +23,7 @@
 	});
 </script>
   
-<nav class="{page.url.pathname === '/' ? 'fixed' : 'sticky'} top-0 w-full z-50  {scrolled ? 'bg-white shadow-md transition-all duration-800' : 'bg-transparent transition-all duration-800'} {isMenuOpen ? 'bg-white shadow-md' : 'bg-transparent'}">
+<nav class="{page.url.pathname === '/' ? 'fixed' : 'sticky'} top-0 w-full z-50  {scrolled ? 'bg-white shadow-md transition-all duration-800' : 'bg-transparent md:transition-all md:duration-800'} {isMenuOpen ? 'bg-white shadow-md transition-none' : 'bg-transparent'}">
 	<div class="container mx-auto px-4 md:px-6">
 	  <!-- Desktop Navigation -->
 	  <div class="hidden md:flex items-center justify-center relative">
@@ -47,7 +47,7 @@
 	      <div class="flex space-x-10 items-center">
 	        <a href="/about" class="text-gray-800 hover:text-gray-600 transition text-lg font-bold whitespace-nowrap">About Us</a>
 	      
-	        <a href="https://www.instagram.com/pier303seafood/" target="_blank" rel="noopener noreferrer" class="text-gray-800 hover:text-pink-600 transition">
+	        <a href="https://www.instagram.com/pier303seafood/" target="_blank" rel="noopener noreferrer" class="text-gray-800 hover:text-pink-600 transition" aria-label="Instagram">
 	          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
 	            <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"/>
 	          </svg>
@@ -89,7 +89,7 @@
 	        <a href="/catering" class="text-gray-800 hover:text-gray-600 transition text-xl font-medium">Catering</a>
 	        <a href="/farmers-market" class="text-gray-800 hover:text-gray-600 transition text-xl font-medium">Farmers Market</a>
 	        <a href="/about" class="text-gray-800 hover:text-gray-600 transition text-xl font-medium">About Us</a>
-	        <a href="https://www.instagram.com/pier303seafood/" class="text-gray-800 hover:text-pink-600 transition flex items-center text-xl font-medium">
+	        <a href="https://www.instagram.com/pier303seafood/" class="text-gray-800 hover:text-pink-600 transition flex items-center text-xl font-medium" aria-label="Instagram">
 	          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 mr-2">
 	            <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"/>
 	          </svg>
@@ -99,6 +99,3 @@
 	  {/if}
 	</div>
 </nav>
-  
-<!-- Spacer to prevent content from being hidden behind the fixed navbar -->
-<!-- <div class="h-16"></div> -->
