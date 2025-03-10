e9eca839e3a7f5896a13005b2741ab8033189ff7
Garik Asplund <garik.asplund@gmail.com>
2025-03-09 21:35:18 -0700

    small tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
-import imageWatcherPlugin from './img-plugin.js'; // Import your custom plugin
+// import imageWatcherPlugin from './img-plugin.js'; // Import your custom plugin
 
 export default defineConfig({
-	plugins: [imageWatcherPlugin(), enhancedImages(), sveltekit(), tailwindcss()],
+	plugins: [
+		// imageWatcherPlugin(),
+		enhancedImages(),
+		sveltekit(),
+		tailwindcss()
+	],
 	optimizeDeps: {
 		// Force Vite to not cache these image-related modules
 		exclude: ['vite-imagetools', 'imagetools-core']
